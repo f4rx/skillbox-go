1ce461ca46be23ddcda443d6437933d72f16101a
Aleksey Stepanenko <stepanenko@selectel.ru>
2020-12-28 14:10:22 +0300

    add level 9

--- 9/6/2.go
+++ 9/6/2.go
@@ -20,31 +20,17 @@ func main() {
 		MinInt32        -2147483648
 		MinInt16        -32768
 		MinInt8         -128
-		MaxInt8         127
 		MaxUint8        255
-		MaxInt16        32767
 		MaxUint16       65535
-		MaxInt32        2147483647
 		MaxUint32       4294967295
 	*/
 
-	/*
-		Я исходил из принадлежности произведения диапазоны из списка выше
-		и поэтому у меня 3000 на 3000 попадёт в int32, а не uint32, как в примере с оценкой.
-		По тексту задачания как я понял важен объем/размер переменной,
-		а у int32 и uint32 размер одинаковый
-	*/
-
 	switch {
-	case result > math.MaxInt32:
-		fmt.Printf("%T\n", uint32(result))
 	case result > math.MaxUint16:
-		fmt.Printf("%T\n", int32(result))
-	case result > math.MaxInt16:
-		fmt.Printf("%T\n", uint16(result))
+		fmt.Printf("%T\n", uint32(result))
 	case result > math.MaxUint8:
-		fmt.Printf("%T\n", int16(result))
-	case result > math.MaxInt8:
+		fmt.Printf("%T\n", uint16(result))
+	case result > 0:
 		fmt.Printf("%T\n", uint8(result))
 	case result > math.MinInt8:
 		fmt.Printf("%T\n", int8(result))
